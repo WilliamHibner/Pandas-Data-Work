846c5a68c99153eda225d73ce89bef760e5a16b7
William Hibner <william.hibner@gmail.com>
2018-12-14 06:10:05 -0600

    Add files via upload

---
+++ HeroesOfPymoli_WDH.py
@@ -0,0 +1,8 @@
+### Heroes Of Pymoli Data Analysis
+# Of the 1163 active players, the vast majority are male (84%). There also exists, a smaller, but notable proportion of female players (14%).
+
+* Our peak age demographic falls between 20-24 (44.8%) with secondary groups falling between 15-19 (18.60%) and 25-29 (13.4%).  
+-----
+
+### Note
+* Instructions have been included for each segment. You do not have to follow them exactly, but they are included to help you think through the steps.
